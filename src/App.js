8bdbda74c447412e36cb7f0af0ccdb4340b3cfce
Lucas.M <lucassss8v@gmail.com>
2022-07-26 09:23:39 -0300

    Finished header layout

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./assets/global.css/reset.css"
+import "./assets/global.css/styles.css"
+import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
+export default function App() {
+    return (
+        <>
+            <Header />
+            <Home />
+        </>
+    )
+}
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import "./styles.css"
+export default function Home(){
+    return (
+       <div className="home">
+           <p>Selecione o filme</p>
+           <div className="movies">
+               <div className="movie"></div>
+               <div className="movie"></div>
+               <div className="movie"></div>
+               <div className="movie"></div>
+           </div>
+
+       </div>
+    )
+}
